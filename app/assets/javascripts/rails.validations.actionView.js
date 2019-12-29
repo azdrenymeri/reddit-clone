383c10e7fa6eb8885cf30c882fc58917f20de079
Azdren <azdreny@gmail.com>
2019-12-29 11:37:29 +0100

    subreddit details partially done

--- app/assets/javascripts/rails.validations.actionView.js
+++ app/assets/javascripts/rails.validations.actionView.js
@@ -1,22 +1,23 @@
 window.ClientSideValidations.formBuilders['ActionView::Helpers::FormBuilder'] = {
     add: function(element, settings, message) {
       // custom add code here
-      element.addClass('is-invalid');
-      const attrVal = element.attr('name');
-      let fieldName = attrVal.slice(attrVal.indexOf('[')+1, attrVal.indexOf(']'));
-    
-      //making the first letter uppercase
-     fieldName =  fieldName.charAt(0).toUpperCase() + fieldName.slice(1)
+        element.addClass('is-invalid');
+        const attrVal = element.attr('name');
+        let fieldName = attrVal.slice(attrVal.indexOf('[')+1, attrVal.indexOf(']'));
+        fieldName = fieldName.split('_').join(' ');
+        //making the first letter uppercase
+        fieldName =  fieldName.charAt(0).toUpperCase() + fieldName.slice(1)
 
-      if(element.parent().has('div').length == 0){
+        if(element.parent().has('div').length == 0){
           element.parent().append('<div class="invalid-feedback">'+ fieldName +' '+message+'</div>')  
-      }
+        }
     },
   
     remove: function(element, settings) {
-      // custom remove code here
-      element.parent().remove('.invalid-feedback');
-      element.removeClass('is-invalid');
-
+        // custom remove code here
+        if(element.parent().has('div').length > 0){
+            element.parent().remove('.invalid-feedback');
+            element.removeClass('is-invalid');
+        }
     }
 }
